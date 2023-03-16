ea09dec926a87aca15dc8d60a1eeedb66136c49a
cydphu <121142759+cydphu@users.noreply.github.com>
2023-03-16 10:47:36 -0500

    changed api keys

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ function displayForecast() {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "80a4d0db16a590f9952e17295643238f";
+  let apiKey = "f8e6a9e3d6fde87cb38868da460b1371";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
   console.log(apiUrl);
@@ -76,7 +76,7 @@ function displayWeatherCondition(response) {
 }
 
 function search(city) {
-  let apiKey = "80a4d0db16a590f9952e17295643238f";
+  let apiKey = "f8e6a9e3d6fde87cb38868da460b1371";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayWeatherCondition);
